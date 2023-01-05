d00efdf69c68fe74c734950a466fb80ac4abecea
niu-grandpa <2864103063@qq.com>
2023-01-05 17:05:49 +0800

    解决 202.快乐数

--- other/202.快乐数✅.js
+++ other/202.快乐数✅.js
@@ -11,16 +11,19 @@
  */
 // n =>getNum(n) =>getNum(getNum(n))
 // 环形链表
+
 var isHappy = function(n) {
     if (n === 1) return true;
     const getNum = n => {
         let sum = 0;
         while (n) {
-            sum += (n % 10) ** 2;
+            sum += (n % 10) ** 2; // 个位数求余结果还是它自己
             n = ~~(n / 10);
         }
         return sum;
     };
+    // 用快慢指针拿平方和做对比，相等就false。实际上就是检查链表有没有环
+    // 最终平方和是否等于1
     let slow = n,
         fast = n;
     while (fast !== 1 && getNum(fast) !== 1) {
