4ae0d7c7e8fa302ee5335fb437a1ecbb9342b4ef
niu-grandpa <2864103063@qq.com>
2023-01-15 22:25:56 +0800

    ✅分发饼干

--- greedy/455.分发饼干✅.js
+++ greedy/455.分发饼干✅.js
@@ -11,20 +11,34 @@
  * @return {number}
  */
 var findContentChildren = function(g, s) {
-  g = g.sort((a,b)=>a-b)
-  s = s.sort((a,b)=>a-b)
-  // 饼干满足胃口尽可能大的小朋友
-  let ret = 0
-  let index = s.length-1
-  for(let i=g.length-1;i>=0;i--){
-    if(index>=0 && s[index]>=g[i]){
-      ret++
-      index--
+    if (!g.length || !s.length) return 0;
+
+    g.sort((a, b) => a - b);
+    s.sort((a, b) => a - b);
+
+    const combine = [...g, ...s];
+    let ans = 0,
+        l = g.length - 1,
+        h = combine.length - 1;
+    while (l >= 0 && h >= g.length) {
+        if (combine[l] <= combine[h]) {
+            h--;
+            ans++;
+        }
+        l--;
     }
-  }
-  return ret
-
-
+    return ans;
+    // 饼干满足胃口尽可能大的小朋友
+    // g.sort((a, b) => a - b);
+    // s.sort((a, b) => a - b);
+    // let ret = 0
+    // let index = s.length-1
+    // for(let i=g.length-1;i>=0;i--){
+    //   if(index>=0 && s[index]>=g[i]){
+    //     ret++
+    //     index--
+    //   }
+    // }
+    // return ret
 };
 // @lc code=end
-
