3051e10fc296862bdede50ab727d8ad81729c61b
niu-grandpa <2864103063@qq.com>
2023-01-24 15:32:29 +0800

    ✅组合总和

---
+++ recursion/39.组合总和✅.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=39 lang=javascript
+ *
+ * [39] 组合总和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    // 排序的复杂度是n*lgn
+    candidates.sort();
+    const ans = [];
+    const backtrack = (temp, sum, i) => {
+        if (sum > target) return;
+        if (sum === target) {
+            temp.push(sum);
+            return;
+        }
+        for (let j = i; j < candidates.length; j++) {
+            const n = candidates[i];
+            if (n + sum > target) continue;
+            temp.push(n);
+            sum += n;
+            backtrack(temp.slice(), sum, j);
+            temp.pop();
+            sum -= n;
+        }
+    };
+    backtrack([], 0, 0);
+    return ans;
+};
+// @lc code=end
