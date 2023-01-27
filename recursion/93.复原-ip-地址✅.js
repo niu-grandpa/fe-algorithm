758bdd10e8cecdabfc0213cebc4b088dddd655a4
niu-grandpa <2864103063@qq.com>
2023-01-27 13:15:55 +0800

    ✅复原-ip-地址

--- recursion/93.复原-ip-地址✅.js
+++ recursion/93.复原-ip-地址✅.js
@@ -10,39 +10,28 @@
  * @return {string[]}
  */
 var restoreIpAddresses = function(s) {
-  // 分割问题
-  // 101023
-  // 1|  12|  101| 后面直接终止
-  // 0|
-  let ret = []
-  let path = []
-  backtrack(0)
-  return ret
-
-  function backtrack(i){
-    if(path.length>4){
-      return
-    }
-    if(path.length===4 && i===s.length){
-      ret.push(path.join('.'))
-      return
-    }
-    // 判断终止条件
-    for(let j=i;j<s.length;j++){
-      // 要得符合ip的判断逻辑
-      const str = s.substr(i,j-i+1)
-      if(Number(str)>255){
-        break
-      }
-      if(str.length>1 && str[0]==='0'){
-        break
-      }
-      path.push(str)
-      backtrack(j+1) //切下一个
-      path.pop()
-    }
-  }
-
+    // 分割问题
+    // 101023
+    // 1|  12|  101| 后面直接终止
+    // 0|
+    const ans = [];
+    const backtrack = (temp, i) => {
+        if (temp.length > 4) return;
+        if (temp.length === 4 && i === s.length) {
+            ans.push(temp.join('.'));
+            return;
+        }
+        for (let j = i; j < s.length; j++) {
+            const str = s.substr(i, j - i + 1);
+            if (parseInt(str) > 255) break;
+            // 0开头的无效地址
+            if (str.length > 1 && str[0] === '0') break;
+            temp.push(str);
+            backtrack(temp.slice(), j + 1);
+            temp.pop();
+        }
+    };
+    backtrack([], 0);
+    return ans;
 };
 // @lc code=end
-
