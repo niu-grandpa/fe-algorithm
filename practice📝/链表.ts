9fe1569d5a5376a1e9bda16fdede8a4db1a31611
niu-grandpa <2864103063@qq.com>
2023-02-02 15:33:39 +0800

    update

--- practice📝/链表.ts
+++ practice📝/链表.ts
@@ -1,4 +1,4 @@
-/**
+/*
  * 单链表
  * 本章内容包括：
  * 1.链表数据结构
@@ -33,6 +33,7 @@ class LinkedList {
       }
       cur.next = node;
     }
+    this.length++;
   }
   // 移除元素
   remove(node: _Node) {
@@ -52,6 +53,7 @@ class LinkedList {
       node.next = cur;
       prev!.next = node;
     }
+    this.length++;
   }
   // 返回元素在链表中的索引
   indexOf(node: _Node) {
@@ -64,7 +66,7 @@ class LinkedList {
   }
   // 返回链表中特定位置的元素
   getNodeAt(index: number) {
-    if (index < 0 || index > this.size()) return undefined;
+    if (index < 0 || index > this.size() - 1) return undefined;
     let cur = this.head;
     for (let i = 0; i < index && cur?.next; i++) {
       cur = cur.next;
@@ -73,7 +75,7 @@ class LinkedList {
   }
   // 从链表特定位置移除一个元素
   removeAt(index: number) {
-    if (index < 0 || index > this.size()) return undefined;
+    if (index < 0 || index > this.size() - 1) return undefined;
     let cur = this.head;
     if (index === 0) {
       this.head = cur?.next;
@@ -85,6 +87,7 @@ class LinkedList {
       }
       prev!.next = cur?.next;
     }
+    this.length--;
   }
   toString() {
     let res = '',
@@ -106,9 +109,87 @@ class _Node {
 }
 const list = new LinkedList();
 
-/**
+/*
  * 双链表
  * 6.循环链表
  * 7.排序链表
  * 8.通过链表实现栈
  */
+class DbNode extends _Node {
+  prev?: _Node;
+  constructor(val: number, next?: _Node, prev?: _Node) {
+    super(val, next);
+    this.prev = prev;
+  }
+}
+//双链表
+class DbLinkedList extends LinkedList {
+  head?: DbNode;
+  tail?: DbNode;
+  constructor() {
+    super();
+    this.tail = undefined;
+  }
+  insert(val: number, index: number) {
+    if (index < 0 || index > this.size() - 1) return false;
+    const node = new DbNode(val);
+    let cur: DbNode | undefined;
+    // 插入头部
+    if (index === 0) {
+      if (!this.head) {
+        this.head = node;
+        this.tail = node;
+      } else {
+        cur = this.head as DbNode;
+        node.next = this.head;
+        cur.prev = node;
+        this.head = node;
+      }
+    } else if (index === this.size() - 1) {
+      // 插入尾部
+      cur = this.tail;
+      cur!.next = node;
+      node.prev = cur;
+      this.tail = node;
+    } else {
+      // 插入任意位置
+      // 获取目标节点的前一个节点和当前节点
+      const prev = this.getNodeAt(index - 1);
+      cur = prev!.next;
+      // 新节点next指向当前节点，当前节点prev指向新节点
+      node.next = cur;
+      cur!.prev = node;
+      // 前一个节点指向新节点
+      prev!.next = node;
+      // 新节点prev指向前一个节点
+      node.prev = prev;
+    }
+    this.length++;
+    return true;
+  }
+  removeAt(index: number) {
+    if (index < 0 || index > this.size() - 1) return undefined;
+    let cur: DbNode | undefined;
+    if (index === 0) {
+      cur = this.head;
+      this.head = cur?.next;
+      // 如果删除后只剩下一项，更新tail
+      if (this.size() === 1) {
+        this.tail = undefined;
+      } else {
+        this.head!.prev = undefined;
+      }
+    } else if (index === this.size() - 1) {
+      cur = this.tail;
+      this.tail = cur?.prev;
+      this.tail!.next = undefined;
+    } else {
+      cur = this.getNodeAt(index);
+      const prev = cur!.prev;
+      prev!.next = cur?.next;
+      (cur?.next as DbNode).prev = prev;
+    }
+  }
+}
+
+// 用链表实现栈
