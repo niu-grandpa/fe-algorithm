5c7789eeaa93e99a95e29836646de6979dffcff5
niu-grandpa <2864103063@qq.com>
2023-02-09 14:33:10 +0800

    ✅合并二叉树

--- tree/617.合并二叉树✅.js
+++ tree/617.合并二叉树✅.js
@@ -18,21 +18,14 @@
  * @param {TreeNode} root2
  * @return {TreeNode}
  */
-var mergeTrees = function(root1, root2) {
-
-  function dfs(root1,root2){
-    if(!root1){
-      return root2
-    }
-    if(!root2){
-      return root1
-    }
-    root1.val += root2.val
-    root1.left =dfs(root1.left,root2.left)
-    root1.right =dfs(root1.right,root2.right)
-    return root1
-  }
-  return dfs(root1,root2)
+var mergeTrees = function (root1, root2) {
+  if (root1 === null && root2 === null) return null;
+  if (root1 === null) return root2;
+  if (root2 === null) return root1;
+  return new TreeNode(
+    root1.val + root2.val,
+    mergeTrees(root1.left, root2.left),
+    mergeTrees(root1.right, root2.right)
+  );
 };
 // @lc code=end
-
