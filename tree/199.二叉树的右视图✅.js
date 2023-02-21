b57f3238d6053ece00522cb01456a2a28cee3eec
niu-grandpa <2864103063@qq.com>
2023-02-21 13:40:46 +0800

    ✅.二叉树的右视图

--- tree/199.二叉树的右视图✅.js
+++ tree/199.二叉树的右视图✅.js
@@ -17,28 +17,23 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var rightSideView = function(root) {
+var rightSideView = function (root) {
   // 每一层最右边的一个值
-  let ret = []
-  if(root===null){
-    return ret
-  }
-  let queue = [root]
-
-  while(queue.length){
-    let len = queue.length
-    while(len--){
-      let node = queue.shift()
-      if(len===0){
-        ret.push(node.val)
-      }
-      node.left && queue.push(node.left)
-      node.right && queue.push(node.right)
+  if (!root) return [];
+  const queue = [root];
+  const ans = [];
+  while (queue.length) {
+    const size = queue.length;
+    let right = 0;
+    for (let i = 0; i < size; i++) {
+      const n = queue.shift();
+      // 右节点总是在最后一个被弹出，因此利用这个特性
+      right = n.val;
+      n.left && queue.push(n.left);
+      n.right && queue.push(n.right);
     }
-    
+    ans.push(right);
   }
-
-  return ret
+  return ans;
 };
 // @lc code=end
-
