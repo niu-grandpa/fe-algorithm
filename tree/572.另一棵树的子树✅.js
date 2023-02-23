2845f287f48061bb5674145b4bc6cbf8513eca5b
niu-grandpa <2864103063@qq.com>
2023-02-23 12:24:51 +0800

    ✅另一棵树的子树

--- tree/572.另一棵树的子树✅.js
+++ tree/572.另一棵树的子树✅.js
@@ -20,30 +20,18 @@
  */
 // react的虚拟dom，树平级对比
 
-var isSubtree = function(root, subRoot) {
+var isSubtree = function (root, subRoot) {
   // 不停地比较， 某一个子树，是不是和subRoot一样
-  if(root===null){
-    return false
+  if (!root || !subRoot) return false;
+  const isSameTree = (root1, root2) => {
+    if (!root1 && !root2) return true;
+    if (!root1 || !root2) return false;
+    if (root1.val !== root2.val) return false;
+    return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+  };
+  if (root.val === subRoot.val) {
+    if (isSameTree(root, subRoot)) return true;
   }
-  if(root.val===subRoot.val){
-    if(isSameTree(root,subRoot)){
-      return true
-    }
-  }
-  return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot)
-};
-
-var isSameTree = function(p, q) {
-  if(p===null&&q===null){
-    return true
-  }
-  if(p===null||q===null){
-    return false
-  }
-  if(p.val!==q.val){
-    return false
-  }
-  return isSameTree(p.left,q.left) && isSameTree(p.right,q.right)
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
 };
 // @lc code=end
-
