780421fbd4c9e32091284823464942ab33074db1
niu-grandpa <2864103063@qq.com>
2023-04-24 21:13:41 +0800

    ✅打家劫舍

---
+++ dynamic/198.打家劫舍✅.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=javascript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var rob = function (nums) {
+  const n = nums.length;
+  const dp = Array(n);
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+  for (let i = 2; i < n; i++) {
+    dp[i] = Math.max(dp[i - 2] + nums[i], dp[i]);
+  }
+  return dp[n - 1];
+};
+// @lc code=end
